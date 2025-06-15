5e97963aab7c9d23b402a83a40b8f9f32639ca72
04Shubhampatil <04Shubhampatil>
2025-06-15 19:56:16 +0530

    error handling
    Signed-off-by: 04Shubhampatil <04Shubhampatil>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import "dotenv/config"
+import "dotenv/config";
 import connectDB from "./db/index.js";
-import express from "express"
-const app = express()
-
+import express from "express";
+const app = express();
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running port ${process.env.PORT}`);
+    });
+  })
 
-
+  .catch((err) => {
+    console.log("MONGONdb Connection faild!!", err);
+  });
+app.on("erroe", (error) => {
+  console.log("Error:", error);
+  throw error;
+});
---
+++ src/utils/Apierror.js
@@ -0,0 +1,31 @@
+
+class ApiError extends Error{
+   Constructor(
+        statusCode,
+        message="Something went wrong",
+        errors=[],
+        stack=""
+     )
+     {
+         super(message);
+        this.statusCode=statusCode;
+        this.data=null
+        this.success=false;
+        this.errors= errors;
+        if (stack) {
+            this.stack=stack;
+         }
+         else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+     }
+       
+    
+}
+
+export default ApiError
+         
+     
+       
+    
+    
